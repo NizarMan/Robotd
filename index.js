31bdc363673b403424dbff1119f3be1d11b08e69
NizarMan <116679186+NizarMan@users.noreply.github.com>
2022-12-08 16:06:33 +0100

    Update index.js

--- index.js
+++ index.js
@@ -25,61 +25,12 @@ process.on('unhandledRejection', e => {
     console.log(e.message);
   });
 
-client.on('ready', () => {
-    
-    setInterval( async () => {
-    client.channels.fetch("1034075084089987084") 
-     .then((channel) => { 
-      const VoiceConnection = joinVoiceChannel({
-       channelId: channel.id, 
-       guildId: channel.guild.id, 
-       adapterCreator: channel.guild.voiceAdapterCreator 
-       });
-    }).catch((error) => { return; });
-    }, 1000)
-});
-
-client.on('ready', () => {
-    
-    setInterval( async () => {
-    client.channels.fetch("1033873110715338752") 
-     .then((channel) => { 
-      const VoiceConnection = joinVoiceChannel({
-       channelId: channel.id, 
-       guildId: channel.guild.id, 
-       adapterCreator: channel.guild.voiceAdapterCreator 
-       });
-    }).catch((error) => { return; });
-    }, 1000)
-});
 
 
 
 
-client.on('guildMemberRemove', async Member => {
-    let ChannelLog = await Member.guild.channels.cache.find(Channel => Channel.id === '1034088795722747964')
-    if(!ChannelLog) return;
-
-    let Embed = new EmbedBuilder()
-       .setAuthor({ name: Member.user.tag, iconURL: Member.user.displayAvatarURL({ dynamic: true }) })
-       .setThumbnail(Member.user.displayAvatarURL({ dynamic: true }))
-       .setDescription(`${Member} has been Leaved Server!`)
-       .addFields({ name: 'Member ID', value: Member.id, inline: true }, { name: 'Member URL', value: `[Add Member](https://www.discord.com/users/${Member.id}) ● [Message Member](https://discord.com/channels/@me/${Member.id})`, inline: true })
-    ChannelLog.send({ embeds: [Embed] })
-})
 
 
-client.on('guildMemberRemove', async Member => {
-    let ChannelLog = await Member.guild.channels.cache.find(Channel => Channel.id === '1005802980467036211')
-    if(!ChannelLog) return;
-
-    let Embed = new EmbedBuilder()
-       .setAuthor({ name: Member.user.tag, iconURL: Member.user.displayAvatarURL({ dynamic: true }) })
-       .setThumbnail(Member.user.displayAvatarURL({ dynamic: true }))
-       .setDescription(`${Member} has been Leaved Server!`)
-       .addFields({ name: 'Member ID', value: Member.id, inline: true }, { name: 'Member URL', value: `[Add Member](https://www.discord.com/users/${Member.id}) ● [Message Member](https://discord.com/channels/@me/${Member.id})`, inline: true })
-    ChannelLog.send({ embeds: [Embed] })
-})
 
 
 
